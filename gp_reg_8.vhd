c5884ebd92283d6bcb6143260f50b1adb72c5381
Nils Petersson <nils.h.petersson@gmail.com>
2014-04-10 17:37:13 +0200

    Swappin' names

--- gp_reg_8.vhd
+++ gp_reg_8.vhd
@@ -1,86 +1,24 @@
--- TestBench Template 
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 
-library ieee;
-USE ieee.std_logic_1164.ALL;
-USE ieee.numeric_std.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-ENTITY gp_reg_8_tb IS
-END gp_reg_8_tb;
+entity gp_reg_8 is
+  Port ( clk,rst,load : in  STD_LOGIC;
+         tal_in : in STD_LOGIC_VECTOR(7 downto 0);
+         tal_ut : out  STD_LOGIC_VECTOR(7 downto 0));
+end gp_reg_8;
+architecture gp_reg_8_ar of gp_reg_8 is
 
-architecture gp_reg_8_tb_ar of gp_reg_8_tb is
-  component gp_reg_8
-    port ( clk,rst,load : in std_logic;
-           tal_in : STD_LOGIC_VECTOR(7 downto 0);
-           tal_ut : out  STD_LOGIC_VECTOR(7 downto 0));
-  end component;
-
-  signal clk : std_logic := '0';
-  signal rst : std_logic := '0';
-  signal load : std_logic := '0';
-  signal tal_in : std_logic_vector(7 downto 0);
-  signal tal_ut : std_logic_vector(7 downto 0);
-  signal tb_running : boolean := true;
-  
-begin  -- gp_reg_8_tb_ar
-
-  uut : gp_reg_8 port map (
-    clk => clk,
-    rst => rst,
-    load => load,
-    tal_in => tal_in,
-    tal_ut => tal_ut);
-
-  clk_gen : process
+begin  -- gp_reg_8_ar
+  process(clk)
   begin
-    while tb_running loop
-      clk <= '0';
-      wait for 20 ns;
-      clk <= '1';
-      wait for 20 ns;
-    end loop;
-    wait;
+    if rising_edge(clk) then
+      if rst = '1' then
+        tal_ut <= "0000000000000000";
+      elsif load = '1' then
+        tal_ut <= tal_in;
+      end if;
+    end if;
   end process;
-
-stimuli_generator : process
-    variable i : integer;
-  begin
-
-    rst <= '1';
-    load <= '0';
-    
-    wait for 100 ns;
-    wait until rising_edge(clk);        -- se till att reset släpps synkront
-    rst <= '0';                         -- med klockan
-    report "Reset released" severity note;
-
-    wait for 100 ns;
-    wait until rising_edge(clk);
-    tal_in <= "10010011";
-    report "Tal skall ej in" severity note;
-
-    wait for 100 ns;
-    wait until rising_edge(clk);
-    tal_in <= "00000000";
-    report "Tal skall ej in" severity note;
-     
-    wait for 100 ns;
-    wait until rising_edge(clk);
-    load <= '1';
-    report "Load high" severity note;
-
-    wait for 100 ns;
-    wait until rising_edge(clk);
-    tal_in <= "10010011";
-    report "Tal skall ej in" severity note;
-    
-    for i in 0 to 50000000 loop         -- Vänta ett antal klockcykler
-      wait until rising_edge(clk);
-    end loop;  -- i
-    
-    tb_running <= false;                -- Stanna klockan (vilket medför att inga
-                                        -- nya event genereras vilket stannar
-                                        -- simuleringen).
-    wait;
-  end process;
-
-end gp_reg_8_tb_ar;
+end gp_reg_8_ar;
