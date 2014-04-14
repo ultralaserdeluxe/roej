2cd237cb7daebef275196865eb2d4b3cf4a65b16
Alexander Yngve <aleyn573@student.liu.se>
2014-04-14 14:33:39 +0200

    Added proper initialization of counter value.

--- counter.vhd
+++ counter.vhd
@@ -11,25 +11,19 @@ entity counter is
     reset : in std_logic;
     enable : in std_logic;
     value : out std_logic_vector(n - 1 downto 0));
---    value : out std_logic_vector(10 - 1 downto 0));
   
 end counter;
     
 architecture counter_ar of counter is
   
-  signal count : std_logic_vector(n - 1 downto 0);
---  signal count : std_logic_vector(10 - 1 downto 0);  
-  signal init : std_logic := '0';
+  signal count : std_logic_vector(n - 1 downto 0) := (others => '0');
   
 begin  -- counter_ar
   
   process(clk) is
   begin
     if rising_edge(clk) then
-      if init = '0' then
-        count <= (count'range => '0');
-        init <= '1';
-      elsif reset = '1' then
+      if reset = '1' then
         count <= (count'range => '0');
       elsif enable = '1' then
         count <= count + 1;  
