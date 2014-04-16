aa76859514be17fe8a14d8106004c8343779567d
Alexander Yngve <aleyn573@student.liu.se>
2014-04-16 18:36:32 +0200

    Minor changes.

--- misc/bmp2slv.py
+++ misc/bmp2slv.py
@@ -2,12 +2,16 @@ import sys
 import Image
 
 def to_binary(number):
-    return bin(number)[2:]
+    return bin(number)[2:].zfill(8)
 
 if __name__ == "__main__":
     im =  Image.open(sys.argv[1])
     binary_numbers =  map(to_binary, list(im.getdata()))
-    print "(",
+
+    printed = 0
+    print "("
     for binary_number in binary_numbers:
         print "\"" + binary_number + "\"" + ",",
+        printed = printed + 1
+        if printed % 16 == 0: print
     print ")"
