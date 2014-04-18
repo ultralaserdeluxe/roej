9d88ddb96a0caa17f5c466c8c57aae6d2b596edc
bombarella <nils.h.petersson@gmail.com>
2014-04-18 18:27:00 +0200

    Micromem

--- gp_reg_16.vhd
+++ gp_reg_16.vhd
@@ -5,8 +5,8 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity gp_reg_16 is
   Port ( clk,rst,load : in  STD_LOGIC;
-         tal_in : in STD_LOGIC_VECTOR(15 downto 0);
-         tal_ut : out  STD_LOGIC_VECTOR(15 downto 0));
+         input : in STD_LOGIC_VECTOR(15 downto 0);
+         output : out  STD_LOGIC_VECTOR(15 downto 0));
 end gp_reg_16;
 architecture gp_reg_16_ar of gp_reg_16 is
 
@@ -15,9 +15,9 @@ begin  -- gp_reg_16_ar
   begin
     if rising_edge(clk) then
       if rst = '1' then
-        tal_ut <= "0000000000000000";
+        input <= "0000000000000000";
       elsif load = '1' then
-        tal_ut <= tal_in;
+        output <= input;
       end if;
     end if;
   end process;
