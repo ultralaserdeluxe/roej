ecd1f13b05664ccd1553c04c2dce0fd3cdce8615
Niklas Pettersson <nikpe353@student.liu.se>
2014-05-16 19:27:03 +0200

    Lots of glorious changes! Assembly code to generate mines is done.

--- k2.vhd
+++ k2.vhd
@@ -12,8 +12,8 @@ begin
   
   k2_out <= "00000101" when k2_in = "000" else 	-- absolut
             "00001111" when k2_in = "001" else 	-- indirekt
-            "00011100" when k2_in = "010" else 	-- indexerad
-            "00101000" when k2_in = "011" else 	-- relativ
+            "00011110" when k2_in = "010" else 	-- indexerad
+            "00101001" when k2_in = "011" else 	-- relativ
             "00110101" when k2_in = "100" else 	-- omedelbar
             "00110111" when k2_in = "101" else 	-- underförstådd
             "00000000";
